c220a14632e9042138ffce672d37c82e04426aea
Shubham6972 <shubhamsonkusare18698@gmail.com>
2024-04-25 17:54:22 +0530

    commit #2

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -32,44 +32,6 @@ export const useGetMyRestaurant = () => {
     return {restaurant,isLoading};
 }
 
-// export const useGetMyRestaurant = () => {
-//     const { getAccessTokenSilently } = useAuth0();
-
-//     const getMyRestaurantRequest = async (): Promise<Restaurant> => {
-//         const accessToken = await getAccessTokenSilently();
-
-//         const response = await fetch(`${API_BASE_URL}/api/my/kitchen`, {
-//             method: "GET",
-//             headers: {
-//                 Authorization: `Bearer ${accessToken}`,
-//             },
-//         });
-
-//         if (!response.ok) {
-//             throw new Error("Failed to get Kitchen");
-//         }
-
-//         const restaurant = await response.json();
-
-//         // Fetch and convert the image to a File object
-//         const imageUrl = restaurant.kitchenImage; // Assuming 'kitchenImage' is the key for the image URL
-//         const imageResponse = await fetch(imageUrl);
-//         const imageBlob = await imageResponse.blob();
-//         const imageFile = new File([imageBlob], 'image.jpg', { type: 'image/jpg' });
-
-//         // Add the imageFile to the restaurant object
-//         return { ...restaurant, kitchenImageFile: imageFile };
-//     };
-
-//     const { data: restaurant, isLoading } = useQuery(
-//         "fetchMyRestaurant",
-//         getMyRestaurantRequest
-//     );
-
-//     return { restaurant, isLoading };
-
-
-
 
 export const useCreateMyRestaurant = () => {
     const { getAccessTokenSilently } = useAuth0();
